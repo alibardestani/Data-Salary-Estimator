e80f6e484bb104f443ea4008525ffa16c11f839c
Ali Bardestani <alibardestani201796@gmail.com>
2024-11-08 12:57:59 +0330

    uplodading data adn data collection script

--- glassdoor_data_scraper.py
+++ glassdoor_data_scraper.py
@@ -5,4 +5,6 @@ import pandas as pd
 
 path = r'C:\Users\My Computer\Desktop\ds_salary_proj\chromedriver'
 
-df = gs.get_jobs('data scientist', 15, False, path, 15)
+df = gs.get_jobs('data scientist', 1000, False, path, 15)
+
+df.to_csv('glassdoor_jobs.csv', index=False)
